89b1d7b4ace82dc1e20680f8f3cb154bad566efd
Davide Garberi <dade.garberi@gmail.com>
2025-01-25 03:18:12 +0100

    src: style: Rework logic

--- src/style.ts
+++ src/style.ts
@@ -1,50 +1,73 @@
-export const cardStyles = {
-    card: [
-      "text-align: center",
-      "padding-bottom: 10px",
-      "font-size: 32px",
-      "font-weight: bold",
-      "width: 100%",
-      "min-height: 5vh",
-      "flex: 0 0 auto"
-    ].join("; ") + ";",
-
-    cardBlue: [
+const styleDef = {
+    cardBase: [
+        "text-align: center",
+        "font-weight: bold",
+        "width: 100%",
+        "flex: 0 0 auto"
+    ],
+    smallCard: [
+        "font-size: 16px",
+        "min-height: 4vh",
+        "padding-top: 1.2vh",
+    ],
+    bigCard: [
+        "font-size: 32px",
+        "font-weight: bold",
+        "min-height: 5vh",
+        "padding-bottom: 1vh",
+    ],
+    fullBlue: [
         "background-color: var(--b3-card-info-background)",
         "color: var(--b3-card-info-color)"
-    ].join("; ") + ";",
-
-    cardGreen: [
+    ],
+    fullGreen: [
         "background-color: var(--b3-card-success-background)",
         "color: var(--b3-card-success-color)"
-    ].join("; ") + ";",
-
-    cardRed: [
-      "background-color: var(--b3-card-error-background)",
-      "color: var(--b3-card-error-color)"
-    ].join("; ") + ";",
-
-    cardYellow: [
-      "background-color: var(--b3-card-warning-background)",
-      "color: var(--b3-card-warning-color)"
-    ].join("; ") + ";",
+    ],
+    fullRed: [
+        "background-color: var(--b3-card-error-background)",
+        "color: var(--b3-card-error-color)"
+    ],
+    fullYellow: [
+        "background-color: var(--b3-card-warning-background)",
+        "color: var(--b3-card-warning-color)"
+    ]
 };
+
+const composeStyles = (...styleGroups: string[][]) =>
+    styleGroups.flat().join('; ') + ';';
 
 export const buttonConfigs = [
     {
-        value: cardStyles.card + cardStyles.cardBlue,
+        value: composeStyles(
+            styleDef.cardBase,
+            styleDef.bigCard,
+            styleDef.fullBlue
+        ),
         label: "blue"
     },
     {
-        value: cardStyles.card + cardStyles.cardGreen,
+        value: composeStyles(
+            styleDef.cardBase,
+            styleDef.bigCard,
+            styleDef.fullGreen
+        ),
         label: "green"
     },
     {
-        value: cardStyles.card + cardStyles.cardRed,
+        value: composeStyles(
+            styleDef.cardBase,
+            styleDef.bigCard,
+            styleDef.fullRed
+        ),
         label: "red"
     },
     {
-        value: cardStyles.card + cardStyles.cardYellow,
+        value: composeStyles(
+            styleDef.cardBase,
+            styleDef.bigCard,
+            styleDef.fullYellow
+        ),
         label: "yellow"
     },
 ];
